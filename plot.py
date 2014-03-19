9ad1be0113bb1e5e2c5160de4f4f1878da2d76bf
Danny Riordan <dannyriordan@gmail.com>
2014-03-19 15:35:23 +0000

    Update plot.py
    added credit

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
-from math import sin
+# Shamelessly cannibalised from http://matplotlib.org/1.3.1/examples/animation/simple_anim.html
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -13,14 +14,13 @@ x = np.arange(linelength)
 line, = ax.plot(data[1])
 
 def animate(i):
-    line.set_ydata(data[i])  # update the data
+    line.set_ydata(data[i])
     return line,
 
-#Init only required for blitting to give a clean slate.
+
 def init():
     y=np.ma.array(x, mask=True)
     line.set_ydata(y)
-    #plt.fill(x, y, 'r')
     return line,
 
 ani = animation.FuncAnimation(fig, animate, np.arange(0, filelength), init_func=init,
